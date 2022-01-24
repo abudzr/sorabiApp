2f4ae50414555ef5320d74dc20aedc88303f39ca
Abu Dzar Al Ghifari <abudzar.alghifari@ikonsultan.com>
2022-01-24 10:17:05 +0700

    first commit

---
+++ src/main/java/com/example/sorabi/SorabiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sorabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorabiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SorabiApplication.class, args);
+    }
+
+}
